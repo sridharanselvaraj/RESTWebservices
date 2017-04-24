19327ef93d7b1becebfc2de723727c4191e35d42
admin <admin@admin-PC.lan>
2017-04-24 19:09:24 +0100

    QueryParam Testing

--- src/main/java/org/sri/java/messenger/service/MessageService.java
+++ src/main/java/org/sri/java/messenger/service/MessageService.java
@@ -1,6 +1,7 @@
 package org.sri.java.messenger.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -22,6 +23,29 @@ public class MessageService {
 	public List<Message> getAllMessage()
 	{
 		return new ArrayList<Message>(messages.values());
+	}
+		
+	public List<Message> getAllMessageForYear(int year)
+
+	{
+		List<Message> messageForYear =new ArrayList<>();
+		Calendar cal=Calendar.getInstance();
+		for(Message message: messages.values())
+		{
+			cal.setTime(message.getCreated());
+			if(cal.get(Calendar.YEAR)==year)
+			{
+				messageForYear.add(message);
+			}
+		}
+		return messageForYear;
+	}
+	
+	public List<Message> getAllMessagePaginated(int start,int size)
+	{
+		ArrayList<Message> list =new ArrayList<Message>(messages.values());
+		if(start+size > list.size()) return new ArrayList<Message>();
+		return list.subList(start, start+size);
 	}
 	
 	public Message getMessage(long id)
